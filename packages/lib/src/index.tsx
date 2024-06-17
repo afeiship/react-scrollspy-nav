396def7952cfe3970fc5e407804a905ff2d7cae2
afeiship <1290657123@qq.com>
2024-06-17 22:04:11 +0800

    feat: add example

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,22 +1,62 @@
-import React, { HTMLAttributes } from 'react';
+// import noop from '@jswork/noop';
 import cx from 'classnames';
+import React, {ReactNode, Component, HTMLAttributes} from "react";
+import ReactList, {TemplateArgs} from '@jswork/react-list';
 
-const CLASS_NAME = 'react-scrollspy-nav';
-
-export type IProps = {
+const CLASS_NAME = "react-scrollspy-nav";
+// const uuid = () => Math.random().toString(36).substring(2, 9);
+export type ReactScrollspyNavProps = {
   /**
-   * Additional CSS class names to apply to the component.
+   * The extended className for component.
    * @default ''
    */
   className?: string;
   /**
-   * The content of the component.
-   * @default null
+   * The className for nav element.
+   * @default ''
    */
-  children?: React.ReactNode;
+  navClassName?: string;
+  /**
+   * The children element.
+   */
+  children?: ReactNode;
+  /**
+   * The items of spy navigation.
+   */
+  items: any[];
+  /**
+   * The template function for rendering items.
+   * @param args
+   */
+  template?: (args: TemplateArgs) => ReactNode;
+  /**
+   * The reverse flag for navigation and children.
+   */
+  reverse?: boolean;
 } & HTMLAttributes<HTMLDivElement>;
 
-export default function ReactScrollspyNav(props: IProps) {
-  const { className, ...rest } = props;
-  return <div data-component={CLASS_NAME} className={cx(CLASS_NAME, className)} {...rest} />;
+export default class ReactScrollspyNav extends Component<ReactScrollspyNavProps> {
+  static displayName = CLASS_NAME;
+  static version = "__VERSION__";
+  static defaultProps = {
+    items: [],
+  };
+
+  get children() {
+    const {children, reverse, items, navClassName, template} = this.props;
+    const views = [
+      <ReactList as="div" className={cx(`${CLASS_NAME}__nav`, navClassName)} items={items} template={template}/>,
+      children
+    ];
+    return reverse ? views.reverse() : views;
+  }
+
+  render() {
+    const {className, children, items, template, navClassName, reverse, ...rest} = this.props;
+    return (
+      <div data-component={CLASS_NAME} className={cx(CLASS_NAME, className)} {...rest}>
+        {this.children}
+      </div>
+    );
+  }
 }
